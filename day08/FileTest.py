e0adb1f01c1ea8ff2a1aa0937a5aa3544b99151f
看看太阳 <aa944589323@gmail.com>
2025-01-16 17:31:08 +0800

    day10

--- day08/FileTest.py
+++ day08/FileTest.py
@@ -1,2 +1,28 @@
 # author: cwb
 # date: 2025/1/16
+import sys ,time, random
+
+
+file = open("file.txt", 'w', encoding="utf-8")
+# r+ 读写模式  w+ 写读模式 a+ 追加模式
+for i in range(100):
+    sys.stdout.write('-')
+sys.stdout.tell()
+for i in range(100):
+    strMsg = "".join( [str(random.randint(i, 1000)), ','])
+    file.write(strMsg)
+    file.flush()
+    sys.stdout.write('*')
+    sys.stdout.flush()
+
+
+file.flush()
+
+file.close()
+
+with open('file1.txt','w') as f, open('file1.txt', 'r') as r:
+    f.write('test')
+    f.flush()
+    str = r.read()
+    print(str)
+print("\n文件操作结束")
